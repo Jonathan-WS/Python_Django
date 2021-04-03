48945afcbb32ffaaca05ab1d973cb44bab472e7a
Jonathan-WS <64705358+Jonathan-WS@users.noreply.github.com>
2021-04-03 21:27:16 +0200

    Add files via upload

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/33760/Desktop/PRACTICE_LINUX_SCRIPTING/Dev/trydjango/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
